a56d79120a2eb41c97de10e7928b2c600341e15b
qp0320fu <qp0320fu@mi.fu-berlin.de>
2025-10-23 00:10:47 +0200

    theme(TodoItem): Improve the display of todo descriptions and subtasks.

--- src/view/Components/Taskview/Todoview/TodoItem.tsx
+++ src/view/Components/Taskview/Todoview/TodoItem.tsx
@@ -3,19 +3,22 @@ import TodoSubTasks from "./TodoSubTasks";
 import renderMarkdown from "../markdownRender"
 import moment from "moment";
 
+
 function TodoItem(props: any) {
-    var dueDate = (props.dueDate==null)?"":("Due Date:"+(moment(props.dueDate).format(props.dueDateFormat)));
+    var dueDate = (props.dueDate == null) ? "" : ("Due Date:" + (moment(props.dueDate).format(props.dueDateFormat)));
     var text_html = renderMarkdown(props.todo_text);
     var note_html = renderMarkdown(props.todo_notes);
     return (
         <div className="todo-item" id={props.id}>
-            <input type="checkbox" className="checkbox" id={props.id} onChange={props.onChange} checked={props.completed}/>
-            <div>
-                <p><span dangerouslySetInnerHTML={{__html: text_html}}></span></p>
-                <div className="description" dangerouslySetInnerHTML={{__html: note_html}}></div>
-                <TodoSubTasks todoID={props.id} subtasks={props.todo_subtasks} onChange={props.onChangeChecklistItem}></TodoSubTasks>
-                <div className="due-date">{dueDate}</div>
+            <div className="todo-text-row">
+                <input type="checkbox" className="checkbox" id={props.id} onChange={props.onChange} checked={props.completed} />
+                <div className="todo-text-container" title={props.todo_text}>
+                    <span dangerouslySetInnerHTML={{ __html: text_html }}></span>
+                </div>
             </div>
+            <div className="description" dangerouslySetInnerHTML={{ __html: note_html }}></div>
+            <TodoSubTasks todoID={props.id} subtasks={props.todo_subtasks} onChange={props.onChangeChecklistItem}></TodoSubTasks>
+            <div className="due-date">{dueDate}</div>
         </div>
     )
 }
