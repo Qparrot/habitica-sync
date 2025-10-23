0e09fbd1e3ea50f58f8c8a86c492d80b75606f44
qp0320fu <qp0320fu@mi.fu-berlin.de>
2025-10-23 21:27:31 +0200

    theme(DailyItem): Display in a line dailysubtask.

--- src/view/Components/Taskview/Dailiesview/DailyItem.tsx
+++ src/view/Components/Taskview/Dailiesview/DailyItem.tsx
@@ -7,15 +7,18 @@ function DailyItem(props: any) {
     var note_html = renderMarkdown(props.daily_notes);
     return (
         <div className="todo-item" id={props.id}>
-            <input type="checkbox" className="checkbox" id={props.id} onChange={props.onChange} checked={props.completed} />
-            <div>
-                <p><span dangerouslySetInnerHTML={{__html: text_html}}></span></p>
-                <div className="description" dangerouslySetInnerHTML={{__html: note_html}}></div>
-                {/* {console.log(props.checklist)} */}
-                <DailySubTasks key={props.daily_subtasks.id} subtasks={props.daily_subtasks} onChangeChecklistItem={props.onChangeChecklistItem}></DailySubTasks>
+            <div className="todo-text-row">
+                <input type="checkbox" className="checkbox" id={props.id} onChange={props.onChange} checked={props.completed} />
+                <div className="todo-text-container" title={props.todo_text}>
+                    <span dangerouslySetInnerHTML={{ __html: text_html }}></span>
+                </div>
             </div>
-
+            <div className="description" dangerouslySetInnerHTML={{ __html: note_html }}></div>
+            {/* {console.log(props.checklist)} */}
+            <DailySubTasks key={props.daily_subtasks.id} subtasks={props.daily_subtasks} onChangeChecklistItem={props.onChangeChecklistItem}></DailySubTasks>
         </div>
+
+
     )
 }
 
